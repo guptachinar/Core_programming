a7d69b38bdaf6b77c49452414ca3ad0207852f84
guptachinar <guptachinar44@gmail.com>
2025-07-31 09:47:10 +0530

    profit calcutor

---
+++ level1/Profircalc.java
@@ -0,0 +1,14 @@
+public class Profitcalc { 
+public static void main(String[] args) { double costPrice = 129; 
+= 
+double selling Price 191; 
+double profit = sellingPrice - costPrice; double profitPercent = (profit / costPrice) System.out.println("The Cost Price is INR 
+Price is 
+INR " + selling Price + 
+* 100; 
++ costPrice + 
+and Selling 
+"\nThe Profit is INR " + profit + " and the Profit Percentage is " + profitPercent 
++ "%"); 
+} 
+} 
