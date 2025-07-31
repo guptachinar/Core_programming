6ea27067efceebbd7c3dd9be81d23072c2a3321c
guptachinar <guptachinar44@gmail.com>
2025-07-31 09:47:55 +0530

    pen division

---
+++ level1/PenDivision.java
@@ -0,0 +1,10 @@
+public class PenDivision { 
+public static void main(String[] args) { 
+int pens 14, students = 3; 
+== 
+int pensPerStudent = pens / students; 
+int remainingPens = pens % students; 
+"1 
+System.out.println("The Pen Per Student is " + pensPerStudent + and the remaining pen not distributed is " + remainingPens); 
+} 
+} 
