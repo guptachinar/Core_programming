188d93815040af607e321a2fb57e9b46cf7980b3
guptachinar <guptachinar44@gmail.com>
2025-07-31 09:48:55 +0530

    course fee

---
+++ level1/CourseFee.java
@@ -0,0 +1,11 @@
+public class CourseFee { 
+public static void main(String[] args) { 
+double fee = 125000; 
+double discountPercent = 10; 
+double discount = (discountPercent / 100) 
+double finalFee = fee - discount; 
+* fee; 
+"I 
+System.out.println("The discount amount is INR " + discount + and final discounted fee is INR " + finalFee); 
+} 
+} 
